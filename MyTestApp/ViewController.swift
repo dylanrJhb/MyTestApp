ea581889562069e7db4d54f6cf8dea54cb2eba6f
Dylan.Reid <dylan.reid@standardbank.co.za>
2023-06-13 13:43:42 +0200

    Color palette done

--- MyTestApp/ViewController.swift
+++ MyTestApp/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let colorWell: UIColorWell = {
+        let colorWell = UIColorWell()
+        colorWell.supportsAlpha = true
+        colorWell.selectedColor = .systemBlue
+        colorWell.title = "Color Well"
+        return colorWell
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        colorWell.backgroundColor = .systemMint
+        view.addSubview(colorWell)
+        
+        colorWell.addTarget(self, action: #selector(colorChanged), for: .valueChanged)
+        
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        colorWell.frame = CGRect(x: 20, y: view.safeAreaInsets.top, width: view.frame.size.width-40, height: 50)
+    }
 
+    @objc private func colorChanged(){
+        view.backgroundColor = colorWell.selectedColor
+    }
+    
 
 }
 
